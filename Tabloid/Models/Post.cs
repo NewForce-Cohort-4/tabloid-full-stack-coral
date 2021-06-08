4da33578f718890432aa85e3469e72b415ce2326
Wes <wrharrison23@gmail.com>
2021-06-08 12:07:59 -0400

    incomplete- pushing to access on laptop

---
+++ Tabloid/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tabloid.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int AuthorId { get; set; }
+        public string AuthorName { get; set; }
+        public string Category { get; set; }
+    }
+}
---
+++ Tabloid/Repositories/PostRepository.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tabloid.Models;
+using Tabloid.Utils;
+
+namespace Tabloid.Repositories
+{
+    public class PostRepository : BaseRepository
+    {
+        public PostRepository(IConfiguration configuration) : base(configuration) { }
+        public Post GetByUserId(int id)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                SELECT Id, Title, DateCreated, UserProfileId,
+                    FROM Post p 
+                    WHERE UserProfileId = @Id
+                    ORDER BY p.DateCreated";
+
+                    DbUtils.AddParameter(cmd, "@Id", id);
+
+                    var reader = cmd.ExecuteReader();
+
+                    Post post = null;
+                    while (reader.Read())
+                    {
+                        post = new Post()
+                        {
+                            Id = DbUtils.GetInt(reader, "PostId"),
+                            Title = DbUtils.GetString(reader, "Title"),
+                            Caption = DbUtils.GetString(reader, "Caption"),
+                            DateCreated = DbUtils.GetDateTime(reader, "PostDateCreated"),
+                            ImageUrl = DbUtils.GetString(reader, "PostImageUrl"),
+                            UserProfileId = DbUtils.GetInt(reader, "UserProfileId"),
+                            UserProfile = new UserProfile()
+                            {
+                                Id = DbUtils.GetInt(reader, "UserProfileId"),
+                                Name = DbUtils.GetString(reader, "Name"),
+                                Email = DbUtils.GetString(reader, "Email"),
+                                DateCreated = DbUtils.GetDateTime(reader, "UserProfileDateCreated"),
+                                ImageUrl = DbUtils.GetString(reader, "UserProfileImageUrl"),
+                            },
+                        };
+                    }
+
+                    reader.Close();
+
+                    return post;
+                }
+            }
+
+        }
+    }
