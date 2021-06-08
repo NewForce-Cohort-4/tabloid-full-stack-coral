7da1fc657af14bed1a1fd88e28c79b6284b735c9
Jon Newton <jonn@hey.com>
2021-06-08 15:05:34 -0400

    Created .NET API files required for listing all approved Posts.

---
+++ Tabloid/Controllers/PostController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.VisualBasic;
+using System;
+using System.Security.Claims;
+using System.Collections.Generic;
+using Tabloid.Models;
+using Tabloid.Repositories;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Tabloid.Controllers
+{
+				[Route("api/[controller]")]
+				[ApiController]
+				[Authorize]
+				public class PostController : ControllerBase
+				{
+								private readonly IPostRepository _postRepository;
+
+								public PostController(IPostRepository postRepository)
+								{
+												_postRepository = postRepository;
+								}
+
+								// GET: api/<ValuesController>
+								[HttpGet]
+								public IActionResult Get()
+								{
+												return Ok(_postRepository.GetAllApprovedPosts());
+								}
+
+								// GET api/<ValuesController>/5
+								[HttpGet("{id}")]
+								public string Get(int id)
+								{
+												return "value";
+								}
+
+								// POST api/<ValuesController>
+								[HttpPost]
+								public void Post([FromBody] string value)
+								{
+								}
+
+								// PUT api/<ValuesController>/5
+								[HttpPut("{id}")]
+								public void Put(int id, [FromBody] string value)
+								{
+								}
+
+								// DELETE api/<ValuesController>/5
+								[HttpDelete("{id}")]
+								public void Delete(int id)
+								{
+								}
+				}
+}
---
+++ Tabloid/Repositories/PostRepository.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Tabloid.Models;
+using Tabloid.Utils;
+
+namespace Tabloid.Repositories
+{
+				public class PostRepository : BaseRepository, IPostRepository
+				{
+								public PostRepository(IConfiguration config) : base(config) { }
+								public List<Post> GetAllApprovedPosts()
+								{
+												using (SqlConnection conn = Connection)
+												{
+																conn.Open();
+																using (SqlCommand cmd = conn.CreateCommand())
+																{
+																				cmd.CommandText = @"
+																							SELECT p.Id, p.Title, p.Content, 
+																														p.ImageLocation AS HeaderImage,
+																														p.CreateDateTime, p.PublishDateTime, p.IsApproved,
+																														p.CategoryId, p.UserProfileId,
+																														c.[Name] AS CategoryName,
+																														u.FirstName, u.LastName, u.DisplayName, 
+																														u.Email, u.CreateDateTime, u.ImageLocation AS AvatarImage,
+																														u.UserTypeId, 
+																														ut.[Name] AS UserTypeName
+																									FROM Post p
+																														LEFT JOIN Category c ON p.CategoryId = c.id
+																														LEFT JOIN UserProfile u ON p.UserProfileId = u.id
+																														LEFT JOIN UserType ut ON u.UserTypeId = ut.id
+																								WHERE IsApproved = 1 AND PublishDateTime < SYSDATETIME()";
+
+																				SqlDataReader reader = cmd.ExecuteReader();
+
+																				List<Post> posts = new List<Post>();
+
+																				while (reader.Read())
+																				{
+																								posts.Add(new Post()
+																								{
+																												Id = DbUtils.GetInt(reader, "PostId"),
+																												Title = DbUtils.GetString(reader, "Title"),
+																												Content = DbUtils.GetString(reader, "Content"),
+																												ImageLocation = DbUtils.GetString(reader, "ImageLocation"),
+																												CreateDateTime = DbUtils.GetDateTime(reader, "CreateDateTime"),
+																												UserProfileId = DbUtils.GetInt(reader, "UserProfileId"),
+																												UserProfile = new UserProfile()
+																												{
+																																Id = DbUtils.GetInt(reader, "UserProfileId"),
+																																FirstName = DbUtils.GetString(reader, "FirstName"),
+																																LastName = DbUtils.GetString(reader, "LastName"),
+																																DisplayName = DbUtils.GetString(reader, "DisplayName"),
+																																Email = DbUtils.GetString(reader, "Email"),
+																																CreateDateTime = DbUtils.GetDateTime(reader, "UserProfileDateCreated"),
+																																ImageLocation = DbUtils.GetString(reader, "ImageLocation"),
+																																UserTypeId = DbUtils.GetInt(reader, "UserTypeId"),
+																												}
+																								});
+																				}
+
+																				reader.Close();
+
+																				return posts;
+																}
+												}
+								}
+				}
+}
